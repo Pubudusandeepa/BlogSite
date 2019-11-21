2577ee317c9a189fc0c0dc2459456fc07b28ec6f
Pubudusandeepa <39061732+Pubudusandeepa@users.noreply.github.com>
2019-11-21 09:29:00 +0530

    Read files in mongodb

--- test.js
+++ test.js
@@ -5,10 +5,20 @@ const Post = require('./database/models/Post')
 mongoose.connect('mongodb://localhost:27017/node-js-test-blog', {useNewUrlParser: true})
 
 
-Post.create({
-    title: 'My first blog post',
-    description: 'Blog post description',
-    content: 'Lorem ipsum content'
-},(error,post) =>{
-    console.log(error,post)
+// Post.find({
+   //title: 'Third My first blog post'
+// }, (error,post)=>{
+//   console.log(error,post)  
+// })
+
+Post.findById("5dd6010a19835e11e8c99619", (error,post)=>{
+  console.log(error,post);
 })
+
+// Post.create({
+//     title: 'Third My first blog post',
+//     description: 'Third Blog post description',
+//     content: 'Third Lorem ipsum content'
+// },(error,post) =>{
+//     console.log(error,post)
+// })
